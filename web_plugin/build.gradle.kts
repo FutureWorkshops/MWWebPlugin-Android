44b28df3e3f9904c73c9a766f229a1848a5fe514
Igor Ferreira <igorcferreira@hotmail.com>
2021-03-02 17:30:14 +0100

    Update plugins to use 0.0.21

--- web_plugin/build.gradle.kts
+++ web_plugin/build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
 
     val fromMaven = project.property("project.mavenCore")?.toString()?.toBoolean() ?: false
     if (findProject(":mw-core") == null || fromMaven) {
-        implementation("com.futureworkshops.mobileworkflow:mw-core:0.0.19")
+        implementation("com.futureworkshops.mobileworkflow:mw-core:0.0.21")
     } else {
         implementation(project(":mw-core"))
     }
