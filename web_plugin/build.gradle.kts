f25be9e05e7301c1aa9846157af9d119c98a49fd
Igor Ferreira <igorcferreira@hotmail.com>
2021-03-10 14:54:46 +0100

    Update core version to 0.0.25

--- web_plugin/build.gradle.kts
+++ web_plugin/build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
 
     val fromMaven = project.property("project.mavenCore")?.toString()?.toBoolean() ?: false
     if (findProject(":mw-core") == null || fromMaven) {
-        implementation("com.futureworkshops.mobileworkflow:mw-core:0.0.24")
+        implementation("com.futureworkshops.mobileworkflow:mw-core:0.0.25")
     } else {
         implementation(project(":mw-core"))
     }
