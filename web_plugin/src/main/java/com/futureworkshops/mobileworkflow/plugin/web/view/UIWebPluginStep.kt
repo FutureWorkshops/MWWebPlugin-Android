def448d59e07091a840d0e8e14a024648d0c7107
Tomer Mandel <tomer@futureworkshops.com>
2021-11-15 16:00:43 +0100

    Removing copy result from previous step to title for steps, except for Display and Network Display steps.

--- web_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/web/view/UIWebPluginStep.kt
+++ web_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/web/view/UIWebPluginStep.kt
@@ -10,17 +10,16 @@ import com.futureworkshops.mobileworkflow.backend.views.step.FragmentStepConfigu
 import com.futureworkshops.mobileworkflow.model.WorkflowServiceResponse
 import com.futureworkshops.mobileworkflow.model.result.StepResult
 import com.futureworkshops.mobileworkflow.services.MobileWorkflowServices
-import com.futureworkshops.mobileworkflow.steps.DataTitle
 import com.futureworkshops.mobileworkflow.steps.Step
 
 internal data class UIWebPluginStep(
-    override val title: String,
+    val title: String,
     private val url: String,
     override var isOptional: Boolean = false,
     override val id: StepIdentifier = StepIdentifier(),
     override val uuid: String,
     private val nextButtonText: String = "Next"
-) : Step, DataTitle {
+) : Step {
 
     override fun createView(
         stepResult: StepResult?,
@@ -37,6 +36,4 @@ internal data class UIWebPluginStep(
         mobileWorkflowServices = mobileWorkflowServices),
         url = url
     )
-
-    override fun copyWithNewTitle(title: String): Step = this
 }
