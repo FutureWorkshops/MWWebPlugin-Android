a6ed50effa91a7b7d0c7507dc823d2a5ada6c2d6
Igor Ferreira <igorcferreira@hotmail.com>
2022-01-10 12:02:10 -0300

    Fix usage of id propery

--- web_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/web/step/WebPluginStep.kt
+++ web_plugin/src/main/java/com/futureworkshops/mobileworkflow/plugin/web/step/WebPluginStep.kt
@@ -6,10 +6,12 @@ package com.futureworkshops.mobileworkflow.plugin.web.step
 
 import android.os.Parcelable
 import com.futureworkshops.mobileworkflow.model.step.PluginStep
+import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class WebPluginStep(
+    override val id: String,
     override val type: String,
     override val title: String,
     val optional: Boolean,
