34ca4f5e6211e86f81e61baec0b135fe29134b0a
Igor Ferreira <me@igorcferreira.dev>
2022-01-18 19:02:17 -0300

    Update dependencies

--- web_plugin/build.gradle.kts
+++ web_plugin/build.gradle.kts
@@ -95,9 +95,9 @@ afterEvaluate {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.5.0")
 
     implementation("io.reactivex.rxjava3:rxjava:3.1.3")
     implementation("io.reactivex.rxjava3:rxandroid:3.0.0")
