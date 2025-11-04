fb1a2c7e6c01c9a1f3ed4e6004a885b75c7bc4d7
Igor Ferreira <me@igorcferreira.dev>
2025-11-04 22:57:34 +0100

    Prepare for Android 15

--- web_plugin/build.gradle.kts
+++ web_plugin/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         }
 
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
